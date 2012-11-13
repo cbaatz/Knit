00f3d510f38a096fa58332a4eb907c20c424710a
Carl Baatz <carl.baatz@gmail.com>
2012-11-13 20:46:00 +0100

    Restructure and clean up slightly.

---
+++ index.js
@@ -0,0 +1,13 @@
+var Tree = require('./lib/structures/tree'),
+    Files = require('./lib/structures/files');
+
+// TODO: Expose more generic tree map/reduce functions that we can use
+// to write file-loaders and asset-tree flatteners.
+exports.tree = function (config) { return new Tree(config); };
+exports.files = function (config) { return new Files(config); };
+
+exports.requestListener = require('./lib/request-listener'); // rL(resources, generatorContext);
+exports.mapNames = require('./lib/map-names'); // mapNames(resources);
+exports.force = require('./lib/force'); // force(generator, callback);
+exports.write = require('./lib/write'); // write(resources, config, callback);
+exports.upload = require('./lib/upload'); // upload(resource, config, callback);
